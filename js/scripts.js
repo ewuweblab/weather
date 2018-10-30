ff47ef00186c4d30388773f12f2e595bf6d2db2b
P. Colin Manikoth <pmanikoth@ewu.edu>
2018-10-30 11:39:43 -0700

    Successfully Retreived Weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-console.log( $() );
 
-console.log( 'OK' );
+$.simpleWeather({
+    location: 98105,
+    success: function(weather) {
+      
+      console.log(weather.image);
+      console.log(weather.city);
+      console.log(weather.temp);
+      console.log(weather.updated);
+       
+      $('.image img').attr( 'src', weather.image  );
+      $('.city').text(weather.city);
+      $('.temp').text(weather.temp);
+      $('.time').text(weather.updated);
+
+
+    }, 
+    error: function(error) {
+      console.log('Go outside!');
+    }
+ });
